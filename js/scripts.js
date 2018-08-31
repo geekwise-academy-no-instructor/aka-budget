6878b36ec6247282af73e93cec99a9fa7efdddd1
Andi Lindblom <andilindblom@gmail.com>
2018-08-30 19:53:13 -0700

    added javascript to access modal HTML nodes. started function to check budget against income

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+// query selecting HTML DOM nodes
+let newBudgetForm = document.forms.newBudgetForm;
+let incomeInput = newBudgetForm.income;
+let rentInput = document.querySelector("#rentInput");
+let billsInput = document.querySelector("#billsInput");
+let groceriesInput = document.querySelector("#groceriesInput");
+let entertainInput = document.querySelector("#entertainInput");
+let personCareInput = document.querySelector("#personCareInput");
+let miscInput = document.querySelector("#miscInput");
+let newBudgetBtn = document.querySelector("#newBudgetBtn");
+
+// declare values of HTML inputs as variables
+let incomeInputVal;
+let rentInputVal;
+let billsInputVal;
+let groceriesInputVal;
+let entertainInputVal;
+let personCareInputVal;
+let miscInputVal;
+let totalBudget;
+let inputArray;
+
+/*
+let augustBudget = {
+	income: incomeInputVal,
+	rent: rentInputVal,
+	bills: billsInputVal,
+	groceries: groceriesInputVal,
+	entertain: entertainInputVal,
+	personCare: personCareInputVal,
+	misc: miscInputVal
+};
+*/
+
+newBudgetBtn.addEventListener("click", e => {
+	incomeInputVal = parseInt(incomeInput.value);
+	rentInputVal = parseInt(rentInput.value);
+	billsInputVal = parseInt(billsInput.value);
+	groceriesInputVal = parseInt(groceriesInput.value);
+	entertainInputVal = parseInt(entertainInput.value);
+	personCareInputVal = parseInt(personCareInput.value);
+	miscInputVal = parseInt(miscInput.value);
+
+	inputArray = [incomeInputVal, rentInputVal, billsInputVal, groceriesInputVal, entertainInputVal, personCareInputVal, miscInputVal];
+	totalBudget = rentInputVal + billsInputVal + groceriesInputVal + entertainInputVal + personCareInputVal + miscInputVal;
+
+	for(index = 0; index < inputArray.length; index++){
+		if(index === !NaN && index >= 0){
+			if(totalBudget === incomeInputVal){
+				console.log("yay!");
+			} else{
+				alert("Your budget doesn't equal your income.");
+			}
+		} else{
+			alert("You didn't enter a valid number.");
+		}
+	};
+});
